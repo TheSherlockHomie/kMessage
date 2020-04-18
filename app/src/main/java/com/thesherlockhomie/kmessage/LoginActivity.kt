4fac1d22e10c2ee7e108c3efed61385ae2bd1a7d
TheSherlockHomie <coolman.kushagra@gmail.com>
2020-04-18 18:30:54 +0530

    Login activity can now return to main activity

--- app/src/main/java/com/thesherlockhomie/kmessage/LoginActivity.kt
+++ app/src/main/java/com/thesherlockhomie/kmessage/LoginActivity.kt
@@ -1,7 +1,9 @@
 package com.thesherlockhomie.kmessage
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
@@ -9,5 +11,16 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        login_btn_login.setOnClickListener {
+            val emailAddrs = email_edittext_login.text.toString()
+            val password = password_edittext_login.text.toString()
+
+            Log.d("LoginActivity", "Email is " + emailAddrs)
+            Log.d("LoginActivity", "Password is $password")
+        }
+
+        register_text_login.setOnClickListener {
+            finish()
+        }
     }
 }
