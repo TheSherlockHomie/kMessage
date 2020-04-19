112a793a713d8f42b388355a00bc160779da7373
TheSherlockHomie <coolman.kushagra@gmail.com>
2020-04-19 22:02:07 +0530

    can now logout!

--- app/src/main/java/com/thesherlockhomie/kmessage/MessagesOverviewActivity.kt
+++ app/src/main/java/com/thesherlockhomie/kmessage/MessagesOverviewActivity.kt
@@ -2,6 +2,8 @@ package com.thesherlockhomie.kmessage
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 
@@ -11,11 +13,46 @@ class MessagesOverviewActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_messages_overview)
 
+        verifyUserLoggedIn()
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_messages_overview, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.newmessage_menu_item -> {
+
+            }
+
+            R.id.logout_menu_item -> {
+                logout()
+            }
+
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
+    private fun verifyUserLoggedIn() {
         val uid = FirebaseAuth.getInstance().currentUser?.uid
         if (uid == null) {
             intent = Intent(this, RegisterActivity::class.java)
             intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
             startActivity(intent)
         }
+    }
+
+    private fun logout() {
+        FirebaseAuth.getInstance().signOut()
+        intent = Intent(this, RegisterActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
     }
 }
