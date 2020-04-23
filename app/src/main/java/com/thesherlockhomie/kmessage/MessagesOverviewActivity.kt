f46f93b20fe2ac2e3ee1c98d498ef7ffb7e42f0b
TheSherlockHomie <coolman.kushagra@gmail.com>
2020-04-23 17:48:25 +0530

    get currentUser as a companion object

--- app/src/main/java/com/thesherlockhomie/kmessage/MessagesOverviewActivity.kt
+++ app/src/main/java/com/thesherlockhomie/kmessage/MessagesOverviewActivity.kt
@@ -2,18 +2,31 @@ package com.thesherlockhomie.kmessage
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class MessagesOverviewActivity : AppCompatActivity() {
+
+    companion object {
+        var user: User? = null
+        val LOGGING_KEY = "MessagesOverviewActivit"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_messages_overview)
 
         verifyUserLoggedIn()
+
+        getCurrentUser()
 
     }
 
@@ -47,6 +60,36 @@ class MessagesOverviewActivity : AppCompatActivity() {
             intent = Intent(this, RegisterActivity::class.java)
             intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
             startActivity(intent)
+        }
+    }
+
+    private fun getCurrentUser() {
+        val uid = FirebaseAuth.getInstance().currentUser?.uid
+        if (uid != null) {
+            var ref = FirebaseDatabase.getInstance().getReference("/users/$uid")
+            ref.addListenerForSingleValueEvent(object : ValueEventListener {
+
+                override fun onDataChange(p0: DataSnapshot) {
+                    user = p0.getValue(User::class.java)
+                    if (user == null) {
+                        Log.e(LOGGING_KEY, "User found was null!")
+                        Toast.makeText(
+                            this@MessagesOverviewActivity,
+                            "USer found was null!",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+
+                override fun onCancelled(p0: DatabaseError) {
+                    Log.e(LOGGING_KEY, "Could not get current user: ${p0.message}")
+                    Toast.makeText(
+                        this@MessagesOverviewActivity,
+                        "Could not get current user: ${p0.message}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            })
         }
     }
 
